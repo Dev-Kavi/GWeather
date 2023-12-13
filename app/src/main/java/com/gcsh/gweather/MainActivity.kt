655efee8444d9ed24d22bb467961c197fb4b704d
devcarl <dev.carllouissevisda@gmail.com>
2023-12-14 01:12:47 +0800

    Initial setup for initial nav graph

--- app/src/main/java/com/gcsh/gweather/MainActivity.kt
+++ app/src/main/java/com/gcsh/gweather/MainActivity.kt
@@ -2,10 +2,20 @@ package com.gcsh.gweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var initialNavController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        initialNavController = findNavController(R.id.initialLoadContainerView)
+        return initialNavController.navigateUp() || super.onSupportNavigateUp()
     }
 }
