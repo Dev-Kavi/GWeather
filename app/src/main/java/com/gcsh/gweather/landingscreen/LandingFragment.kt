e51c069e4823967694affb93635ce2b0719d8c12
devcarl <dev.carllouissevisda@gmail.com>
2023-12-14 01:12:03 +0800

    Initial setup for landing screen

---
+++ app/src/main/java/com/gcsh/gweather/landingscreen/LandingFragment.kt
@@ -0,0 +1,23 @@
+package com.gcsh.gweather.landingscreen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gcsh.gweather.R
+
+class LandingFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_landing, container, false)
+    }
+
+}
