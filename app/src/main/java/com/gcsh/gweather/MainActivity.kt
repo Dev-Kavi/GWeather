c743ab9f280cf1a73df223adeeb5e8feb59a755a
devcarl <dev.carllouissevisda@gmail.com>
2023-12-13 22:10:04 +0800

    Initial commit for GWeather

---
+++ app/src/main/java/com/gcsh/gweather/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gcsh.gweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
